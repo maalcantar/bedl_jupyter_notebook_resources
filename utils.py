4e51fc679a5e1224b7f1d7a755ad2c14d9423515
Jackie Valeri <jackievaleri8@gmail.com>
2021-08-18 20:36:21 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # Note: In order to import from utils with ‘from utils import *’, the utils.py file needs to be in the same folder as the notebook (at least as its defined now). 
 # If the utils.py file is not in the same folder, you can use sys to append a path for the file.
+# See https://www.johnny-lin.com/cdat_tips/tips_pylang/path.html for an example.
 
 def add(x, y):
 	"""A function that adds two numbers and prints the output.
@@ -8,6 +9,6 @@ def add(x, y):
 	x -- a number
 	y -- another number
 	"""
-	
+
 	summed = float(x) + float(y)
 	print("the sum of " + str(x) + " and " + str(y) + " is : " + str(summed))
