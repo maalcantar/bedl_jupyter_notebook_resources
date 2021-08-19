6f856bf62578fb285ae4ae1c453d15182a2ce6aa
Jackie Valeri <jackievaleri8@gmail.com>
2021-08-18 20:29:10 -0400

    Update utils.py
    https://github.com/maalcantar/bedl_jupyter_notebook_resources/issues/5#issue-901095098

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+# Note: In order to import from utils with ‘from utils import *’, the utils.py file needs to be in the same folder as the notebook (at least as its defined now). 
+# If the utils.py file is not in the same folder, you can use sys to append a path for the file.
+
 def add(x, y):
 	summed = float(x) + float(y)
 	print("the sum of " + str(x) + " and " + str(y) + " is : " + str(summed))
